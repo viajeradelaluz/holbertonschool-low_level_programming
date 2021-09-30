f31ba49be53f5282fd5cd55b1a04b618c32e98cb
viajeradelaluz <jonna.alfonso@gmail.com>
2021-09-30 16:37:15 -0700

    Print the sizeof() in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,4 +11,5 @@ int main(void)
 	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	return (0);
 }
