aae6bf1dd1caa034d2c6d623087b9cc2512c8cdf
viajeradelaluz <jonna.alfonso@gmail.com>
2021-10-01 19:18:34 -0700

    Print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in reverse
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z >= 'a'; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
+
