7cba31b73dd0d56af1c3e7554c35881432759f6c
viajeradelaluz <jonna.alfonso@gmail.com>
2021-10-01 18:15:35 -0700

    Print some alphabet letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print some letters of the alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char apb;
+
+	for (apb = 'a'; apb <= 'z' && != 'e' 'q'; apb++)
+	{
+		putchar(apb);
+	}
+	putchar('\n');
+	return (0);
+}
