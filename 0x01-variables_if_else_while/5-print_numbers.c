3eca0d271e01e433af4646ceef1b477d33d22916
viajeradelaluz <jonna.alfonso@gmail.com>
2021-10-01 18:48:08 -0700

    Print numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+ * main - Print numbers base 10
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
