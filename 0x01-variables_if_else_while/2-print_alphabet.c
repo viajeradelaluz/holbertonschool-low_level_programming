45c434692a1ea8dca1e678f1364612b23a8d4a37
viajeradelaluz <jonna.alfonso@gmail.com>
2021-10-01 17:27:24 -0700

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char apb;
+
+	for (apb = 'a'; apb <= 'z'; apb++)
+	{
+		putchar(apb);
+	}
+	return (0);
+}
