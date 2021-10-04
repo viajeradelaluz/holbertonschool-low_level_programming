7e2f14719724cca726b886753ea6babb12957b20
viajeradelaluz <jonna.alfonso@gmail.com>
2021-10-04 10:55:02 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - Print _putchar followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char p = '_putchar';
+
+	putchar(p);
+	putchar('\n');
+	return(0);
+}
