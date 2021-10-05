3769ec0565db7b05358ed34279f3989abc907609
viajeradelaluz <jonna.alfonso@gmail.com>
2021-10-04 20:24:47 -0700

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Add two integers
+ * @n: First input in the add
+ * @g: Second input in the add
+ * Return: Add product
+ */
+int add(int n, int g)
+{
+	return (n + g);
+}
