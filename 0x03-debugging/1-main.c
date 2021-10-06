66601a227c19e31da042b7fa31abc85da479b8b6
viajeradelaluz <jonna.alfonso@gmail.com>
2021-10-06 10:21:25 -0700

    Cooment out the code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	/** int i;
-	 *
+	int i;
+	/**	
 	 * printf("Infinite loop incoming :(\n");
 	 *
 	 * i = 0;
@@ -17,8 +17,8 @@ int main(void)
 	 * {
 	 *	putchar(i);
 	 * }
+	 *
+	 * printf("Infinite loop avoided! \\o/\n");
 	 */
-	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
