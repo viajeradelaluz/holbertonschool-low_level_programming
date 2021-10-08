0f051de4b665cd67ea6cfe25c87d1a57884b00c2
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-10-08 17:32:40 -0500

    Print multiples of three, five and both

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print multiples of three, five and both.
+ * Return: 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
