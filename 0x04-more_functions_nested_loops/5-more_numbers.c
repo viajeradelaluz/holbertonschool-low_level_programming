7643fdd6c784c177f44f18194ae8b3e4139c9988
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-10-08 16:00:29 -0500

    Print numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Print numbers from 0 to 14.
+ * Return: No return.
+ */
+void more_numbers(void)
+{
+	int c, l;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (l = 0; l <= 14; l++)
+		{
+			if (l >= 10)
+			{
+				putchar((l / 10) + '0');
+			}
+			putchar((l % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
