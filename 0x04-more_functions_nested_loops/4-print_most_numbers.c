36a85225e8ea3627df39a59976ff83b11dc987d0
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-10-08 15:43:15 -0500

    Print less numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - Print less numbers from 0 to 9.
+ * Retur: String of numbers.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			putchar(n + '0');
+		}
+	}
+	putchar('\n');
+}
