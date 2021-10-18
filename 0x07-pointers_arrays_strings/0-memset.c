8f6b7192916576ed5dd2f5cc8b8f540a74973c49
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-10-18 16:44:57 -0500

    Fill the first n bytes of the memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * _memset - Fill the first n bytes of the memory.
+ * @n: Bytes to fill.
  * @s: String input.
  * @b: Constar byte to replace.
+ * Return: String filled.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
