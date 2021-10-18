49307319f2f7469f2d0af4d7ab2ab2cf7f12bfad
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-10-18 18:27:43 -0500

    Search the occurrence of a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - Search the occurrence of a character.
+ * @s: String in which to search.
+ * @c: Character searched.
+ * Return: Pointer on the first occurrence.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
