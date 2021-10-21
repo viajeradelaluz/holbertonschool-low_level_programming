a5b62819d60605964bf408676f70b4dcd5662364
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-10-20 22:13:02 -0500

    Print a string with recursion}

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s), s++;
-		_puts_recursion(s);
+		_putchar('\n');
 	}
 	else
-		_putchar('\n');
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
