1545f20ee7f2dc40795a694595323c9be471baa0
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-10-22 12:29:24 -0500

    Print the program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Print the name of the program
+ * @argc: Argument's counter.
+ * @argv: Argument's strings.
+ * Return: Program's name.
+ */
+int main (int argc, char *argv[])
+{
+	if (argc == 1)
+		printf("%s\n", *argv);
+	return (0);
+}
