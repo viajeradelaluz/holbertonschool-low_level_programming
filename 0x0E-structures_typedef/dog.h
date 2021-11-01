b99dd2cd9242dd224fb1fa48c34305928967cb35
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-01 18:35:49 -0500

    Add dog library

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - ID for dogs.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
