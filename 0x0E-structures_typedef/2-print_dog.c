e0866fbfbcfe1526af83cab224d1d2e5f6ffc072
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-01 19:43:16 -0500

    See dog's information

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - See dog's information.
+ * @d: dog's information.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf(" ");
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
