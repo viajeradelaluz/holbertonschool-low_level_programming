9691ca31cbf074ba86128bae2e196b993f44318c
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-03 16:06:09 -0500

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - Print a string-name.
+ * @name: string-name of the person.
+ * @f: pointer function.
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL && f == NULL)
+		return;
+	else
+		f(name);
+}
