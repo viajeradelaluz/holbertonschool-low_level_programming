87c90c0fe67271eaad3529b80884b5a3d4209fe7
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-03 15:37:31 -0500

    Add the library of the project

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNTION_POINTERS
+#define FUNTION_POINTERS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /* FUNTION_POINTERS */
