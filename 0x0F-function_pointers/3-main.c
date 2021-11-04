915e2fc7bff206b817c232349c04ce61dac13b12
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-03 21:44:39 -0500

    Add main to the project

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Performs simple operations.
+ * @argc: number of parameters.
+ * @argv: input numbers.
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]), b = atoi(argv[3]), result;
+	int (*f)(int, int);
+
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	result = f(a, b);
+	printf("%d\n", result);
+	return (0);
+}
