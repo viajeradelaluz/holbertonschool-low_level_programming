b49074b48442021a25de18e85f9ba4a41aedc336
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-03 22:17:58 -0500

    Add main to the project

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]), b = atoi(argv[3]), result;
+	int a = atoi(argv[1]), b = atoi(argv[3]);
 	int (*f)(int, int);
 
 	if (argc != 4)
@@ -17,18 +17,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (strlen(argv[2]) > 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	f = get_op_func(argv[2]);
 	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = f(a, b);
-	if (result == -1)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", result);
+	printf("%d\n", f(a, b));
 	return (0);
 }
