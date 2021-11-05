a1df0933c25c2d1190b6b2a5dfd56b0cadb40eba
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-04 21:44:26 -0500

    Print any data type

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+/**
+ * print_all - Print any data type.
+ * @format: type of data.
+ * Return: nothing.
+ */
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	char *sval;
+	int counter = 0;
+
+	va_start(args, format);
+	while (format && format[counter])
+	{
+		switch (format[counter])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 's':
+			sval = va_arg(args, char *);
+			if (!sval)
+				printf("(nil)");
+			printf("%s", sval);
+			break;
+		default:
+			counter++;
+			continue;
+		}
+		if (format[counter + 1] != '\0')
+			printf(", ");
+		counter++;
+	}
+	printf("\n");
+	va_end(args);
+}
