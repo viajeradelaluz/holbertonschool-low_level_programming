2f0a3fcf01ba64a4d71447f152ec6d4ac7e44cec
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-16 13:00:07 -0500

    Fix head is NULL statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp_node = *head;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if (new_node == NULL || *head == NULL)
+	if (new_node == NULL || head == NULL)
 		return (NULL);
 
 	new_node->n = n;
