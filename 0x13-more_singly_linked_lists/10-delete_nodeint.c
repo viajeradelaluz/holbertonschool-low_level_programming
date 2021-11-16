d78ce717ef9830fe1448b7125467e3f0b02c5994
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-16 13:05:04 -0500

    Fix index subtraction in while condition

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index != 0)
 	{
-		while (--index)
+		while (index--)
 		{
 			previous = current;
 			current = current->next;
