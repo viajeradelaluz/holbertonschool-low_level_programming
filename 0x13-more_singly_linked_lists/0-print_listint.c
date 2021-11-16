ae314c94ba1bb85ecd7c193027273e8c1561c4c6
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-16 09:26:36 -0500

    Delete NULL at while declaration and swich num_nodes variable position

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		num_nodes++;
 		h = h->next;
+		num_nodes++;
 	}
 	return (num_nodes);
 }
