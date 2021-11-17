ebc5240a6168e0d51d9fed614dadf3d24e313372
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-17 15:48:53 -0500

    Fix the num_nodes variable position

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,12 +18,12 @@ size_t print_listint_safe(const listint_t *head)
 		tail = new_head;
 		printf("[%p] %d\n", (void *)new_head, new_head->n);
 		new_head = new_head->next;
-		num_nodes++;
 		if (tail < new_head)
 		{
 			printf("-> [%p] %d\n", (void *)new_head, new_head->n);
 			break;
 		}
+		num_nodes++;
 	}
 	return (num_nodes);
 }
