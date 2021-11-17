1d105dcee315095f67c8847e3e846e5f6ece5187
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-17 17:35:58 -0500

    Fix node circular error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,18 @@
 #include "lists.h"
+
+/* size_t if_circular(const listint_t *h)
+{
+	const listint_t *tail = h, *head = h;
+
+	while (tail && head && head->next)
+	{
+		tail = tail->next;
+		head = head->next->next;
+		if (tail == head)
+			return (1);
+	}
+	return (0);
+} */
 /**
  * print_listint_safe - Print a single and circular linked list.
  * @head: Points to the first node of the linked list.
@@ -17,7 +31,12 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *)new_head, new_head->n);
 		new_head = new_head->next;
 
-		if (tail < new_head)
+		if (tail < new_head && tail->next == new_head)
+		{
+			printf("-> [%p] %d\n", (void *)new_head, new_head->n);
+			break;
+		}
+		if (tail == tail->next)
 		{
 			printf("-> [%p] %d\n", (void *)new_head, new_head->n);
 			break;
