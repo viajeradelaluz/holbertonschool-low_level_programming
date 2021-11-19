60cef9bd9f053a95f0b06eea762e0f96a9be75e9
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-18 21:09:56 -0500

    Print the binary number of an integer

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_binary - Print the binary number of an integer.
+ * @n: integer to print.
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0 || n >> 1)
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}
