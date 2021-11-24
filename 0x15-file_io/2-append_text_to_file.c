2281488fb637a286b84d58bdb436f44e308e3129
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-23 19:22:08 -0500

    Fix file_write conditional

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[_strlen])
 			_strlen++;
 		file_write = write(file_append, text_content, _strlen);
-		if (file_write)
+		if (!file_write)
 			return (-1);
 	}
 	close(file_append);
