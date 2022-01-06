4e57520d5ab47ef6073b19e0c128939bae8dad69
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-06 13:20:08 -0500

    feat: print all nodes of a linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print all elements of a linked list
+ * @h: points to the start of the linked list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int counter = 0
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
