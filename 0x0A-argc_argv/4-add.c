435ed0702ba3e888f1423b8357f3d8fb96bf0d8e
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-06 22:02:05 -0500

    fix: manage the *argv special characters in a single function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,18 @@
 #include "main.h"
 #include <ctype.h>
 /**
+ * _argv - Evaluate *argv for special characters.
+ * @s: string input.
+ * Return: string evaluated.
+ */
+int _argv(char *s)
+{
+	if (!isdigit(*s))
+		return(*s++);
+	printf("Error\n");
+	return (1);
+}
+/**
  * main - Adds positive numbers.
  * @argc: Argument's counter.
  * @argv: Argument's strings.
@@ -8,20 +20,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int c, s, add = 0;
+	int c, add = 0;
 
-	for (c = 1; c < argc; c++)
+	if (argc > 1)
 	{
-		for (s = 0; argv[c][s] != '\0'; s++)
+		for (c = 1; c < argc; c++)
 		{
-			if (!isdigit(argv[c][s]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+ 			if (atoi(argv[c]))
+				add += atoi(argv[c]);
+			else if (!isdigit(*argv[c]))
+				return (_argv(*argv));
+			add += atoi(argv[c]);
 		}
-		add += atoi(argv[c]);
+		printf("%d\n", add);
 	}
-	printf("%d\n", add);
+	else
+		printf("%d\n", 0);
 	return (0);
 }
