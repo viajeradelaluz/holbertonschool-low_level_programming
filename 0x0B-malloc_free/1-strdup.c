4b51c11d8312be27271722fbe2d48c53054cba50
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-06 22:04:39 -0500

    feat: add comments to the _strdup function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,18 @@ char *_strdup(char *str)
 	char *dup_str, *make_dup;
 
 	if (str != NULL)
-	{
+	{	/* Get the length of str */
 		while (str[str_size])
 			str_size++;
 	}
 	else
 		return (NULL);
-
+	/* Allocate memory for duplicate */
 	dup_str = malloc(sizeof(char) * str_size + 1);
-
+	/* Always ask NULL return for the malloc function */
 	if (dup_str == NULL)
 		return (NULL);
-
+	/* Copy the string with the 2nd. pointer */
 	make_dup = dup_str;
 	while (*str)
 	{
